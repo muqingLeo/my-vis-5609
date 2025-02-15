2a1f4ddb1a971206c7bdf2e0f31d42aeea0a4e68
Muqing Liu <liu01693@umn.edu>
2025-02-15 15:41:48 -0600

    commit for A1 at 3:42

--- my-vis-5609/src/lib/index.ts
+++ my-vis-5609/src/lib/index.ts
@@ -1,1 +1,5 @@
-// place files you want to import through the `$lib` alias in this folder.
+import Bar from './Bar.svelte'
+import Line from './Line.svelte'
+import Heatmap from './Heatmap.svelte'
+
+export {Bar, Line, Heatmap}
