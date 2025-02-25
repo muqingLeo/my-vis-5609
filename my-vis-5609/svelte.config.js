2558b0a309a5979b5fb6c77f8c3937d33c113a72
Muqing Liu <liu01693@umn.edu>
2025-02-24 19:59:43 -0600

    Trying to fix no workflow being run issue

--- my-vis-5609/svelte.config.js
+++ my-vis-5609/svelte.config.js
@@ -7,7 +7,7 @@ const config = {
             fallback: '404.html',
         }),
         paths: {
-            base: process.argv.includes('dev') ? '' : '/my-vis-5609',
+            base: process.argv.includes('dev') ? '' : process.env.BASE_PATH || '/my-vis-5609',
         },
     },
 }
